8f1751a6f474f4cd261364075c30f654f63ed833
Maximiliano Figueroa <111710424+Maxifigueroa20@users.noreply.github.com>
2026-01-16 05:22:39 -0300

    Initial commit

---
+++ src/client/java/com/maxi/simplesbutils/SimpleSbUtilsClient.java
@@ -0,0 +1,10 @@
+package com.maxi.simplesbutils;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SimpleSbUtilsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
